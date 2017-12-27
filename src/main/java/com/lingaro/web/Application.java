e4e40b948e1c5d3b8b7a4ad07a072ee72a64b02d
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2017-12-27 18:57:16 +0100

    show info on /

--- src/main/java/com/lingaro/web/Application.java
+++ src/main/java/com/lingaro/web/Application.java
@@ -1,7 +1,10 @@
 package com.lingaro.web;
 
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
-	@GetMapping("/")
-	public String hello() {
-		return "hello";
+  @GetMapping(value="/", produces=MimeTypeUtils.TEXT_PLAIN_VALUE)
+	public Object info() {
+    return System.getProperties().entrySet()
+      .stream()
+      .map(entry -> entry.getKey()+": "+entry.getValue())
+      .sorted()
+      .collect(Collectors.joining("\n"));
 	}
 
 	public static void main(String[] args) {
