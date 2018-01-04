fbc93ea094df1ae5862bcf09f6ff489cc8dd6dfa
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-01-04 10:30:37 +0100

    tomcat

--- src/main/java/com/lingaro/web/Application.java
+++ src/main/java/com/lingaro/web/Application.java
@@ -5,13 +5,15 @@ import java.util.stream.Collectors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
   private static final Logger LOG = Logger.getGlobal();
 
   @GetMapping(value="/", produces=MimeTypeUtils.TEXT_PLAIN_VALUE)
@@ -23,6 +25,11 @@ public class Application {
       .sorted()
       .collect(Collectors.joining("\n"));
 	}
+  
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(Application.class);
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
